a2f5c9969edc0749b71d2f68427fc67c052a06f1
ralfhei <ralf.heinsoo@gmail.com>
2020-10-02 11:59:26 +0300

    changes

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import('@/assets/styles/index.css');
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import VueRouter from 'vue-router'
 
+Vue.use(VueRouter)
 Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
