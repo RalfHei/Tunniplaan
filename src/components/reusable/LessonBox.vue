8367a50138e582bb5c088fd767d71be744d4043c
TimTimDev <timo.pirn@gmail.com>
2020-10-02 11:53:50 +0300

    tim

---
+++ src/components/reusable/LessonBox.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex justify-center">
+    <div class="h-16 w-11/12 bg-white rounded-lg mt-4 flex flex-row">
+      <div class="text-gray-700 text-center px-2 m-2">8.00-<br>10.00</div>
+      <div class="text-gray-700 text-center px-4 m-2">Ripsmetehnika</div>
+      <div class="text-gray-700 text-center m-2"><p>A. Naulainen K 206</p></div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
