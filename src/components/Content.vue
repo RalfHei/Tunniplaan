578072f7f17f759da0959cf34d4007b8d7c8f3cf
ralfhei <ralf.heinsoo@gmail.com>
2020-10-02 08:17:12 +0300

    first commit

---
+++ src/components/Content.vue
@@ -0,0 +1,56 @@
+<template>
+<div class="w-screen h-screen z-10 overflow-x-hidden">
+  <hooper class="w-screen h-screen" :navButtons ="false" :slidesToShow = 1>
+    <slide class="slide w-screen h-screen bg-yellow-300">
+        <tr v-for=" c in codes" :key="c">
+          <td>{{c}}</td>
+          </tr>
+    </slide>
+    <slide class="slidew-screen h-screen bg-green-400">
+      <h3>Hello2</h3>
+    </slide>
+    <slide class="slide w-screen h-screen bg-orange-200">
+      <h3>Hello3</h3>
+    </slide>
+    </hooper>
+    </div>
+</template>
+
+<script>
+import { Hooper, Slide } from 'hooper';
+import 'hooper/dist/hooper.css';
+export default {
+  components: {
+    Hooper,
+    Slide
+  },
+  mounted() {
+    this.getData();
+    console.log(this.codes)
+  },
+  data: () => ({
+    codes: [],
+  }),
+  methods: {
+     async getData(){
+      try {
+        const api = 'https://tahvel.edu.ee/hois_back/timetables/group/38/432?fbclid=IwAR3EsUV0GpdyImxKCMrlpezjKu2Z9buy3MrkBzBUGQhFXDliwsCWf3yVoEY'
+        this.axios.get(api).then((response) => {
+          for(var d in response.data) {
+            this.codes.push(response.data[d].groupCode)
+          }
+          })
+      } catch(err){
+        console.log(err)
+      }
+
+      }
+
+            
+  }
+}
+</script>
+
+<style>
+
+</style>
